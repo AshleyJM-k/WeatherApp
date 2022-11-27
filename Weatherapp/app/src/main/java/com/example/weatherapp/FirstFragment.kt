5500f7184369d614ca483b5467712bcbafc81dd1
Ashley McGraw-Korthals <101578859+AshleyJM-k@users.noreply.github.com>
2022-11-27 17:09:20 -0500

    Update FirstFragment.kt

--- Weatherapp/app/src/main/java/com/example/weatherapp/FirstFragment.kt
+++ Weatherapp/app/src/main/java/com/example/weatherapp/FirstFragment.kt
@@ -8,15 +8,12 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.weatherapp.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
+
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+   
     private val binding get() = _binding!!
 
     override fun onCreateView(
